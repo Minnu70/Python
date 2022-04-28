f3afca678c1aa60b23e317728c332553bb37c19b
sarath <iamsarathchandra.8@gmail.com>
2022-04-28 08:35:33 +0530

    a functionality to add two numbers

--- one.py
+++ one.py
@@ -1,1 +1,10 @@
-print("Hai i am the one who is lerning data science")
+def add(a,b):
+  if a>b:
+    c = a+b 
+    return c
+  else :
+    return a*a
+print("I am outside the scope of the function")
+
+print(add(1,3))
+print(add(22,3))
